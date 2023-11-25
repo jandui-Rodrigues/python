c8dcd8fe110f1fe8f16f95526283e36cadaab359
jandui <janduineto18@gmail.com>
2023-11-24 21:32:08 -0300

    Complecidade quadratica for dentro do outro

---
+++ dia_04_algoritimos/complexidade_quadratica.py
@@ -0,0 +1,27 @@
+# “tempo de execução dos algoritmos cresce a taxas diferentes”
+
+# Os arrays têm sempre o mesmo tamanho
+# Um crecimento exponencial
+
+# a complexidade desse algoritmo é dada por O(n²) e a chamamos
+# de Complexidade Quadrática.
+
+
+def multiply_arrays(array1, array2):
+    result = []
+    number_of_iterations = 0
+
+    for number1 in array1:
+        print(f"Array 1: {number1}")
+        for number2 in array2:
+            print(f"Array 2: {number2}")
+            result.append(number1 * number2)
+            number_of_iterations += 1
+
+    print(f"{number_of_iterations} iterações!")
+    return result
+
+
+meu_array = [1, 2, 3, 4, 5]
+
+multiply_arrays(meu_array, meu_array)
