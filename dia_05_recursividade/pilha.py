693c093a4dba4e52e58c938a72851a427a3bf8f9
jandui <janduineto18@gmail.com>
2023-11-27 20:48:12 -0300

    entendendo o conceito de empilhamento

---
+++ dia_05_recursividade/pilha.py
@@ -0,0 +1,22 @@
+def saudacao():
+    print("Oi")
+
+
+def despedida():
+    print("Tchau")
+
+
+def init():
+    print("Inicio")
+    saudacao()
+    print("Fim")
+    despedida()
+
+
+init()
+"""
+Inicio
+Oi
+Fim
+Tchau
+"""
